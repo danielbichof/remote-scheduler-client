ccde5161bcd487af90366ddbbef5dd79e539ac92
danielbichof <daniel.h.bichof@gmail.com>
2025-09-17 19:30:14 -0300

    fix: corrigir importação do arquivo App.css no main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -2,6 +2,7 @@ import { RouterProvider, createRouter } from '@tanstack/react-router'
 import { StrictMode } from 'react'
 import ReactDOM from 'react-dom/client'
 import './index.css'
+import './App.css'
 
 // Import the generated route tree
 import { routeTree } from './routeTree.gen'
